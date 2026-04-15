fa419d5db2c65daa4c483e452637fbbc9c2cea4e
DESKTOP-NQFA11O\YOI <j21801796147258@gmail.com>
2026-04-16 01:01:15 +0800

    創建LoginService

---
+++ git協作練習/Login/LoginService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git協作練習.Login
+{
+    internal class LoginService
+    {
+    }
+}
