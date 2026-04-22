6c4e2456fa5ad70cff44bcbde35cb0e888c38b62
周政輝 <leo604leo604@gmail.com>
2026-04-23 00:27:37 +0800

    新增註冊帳號時需要發送Email認證信

--- git協作練習/Login/LoginService.cs
+++ git協作練習/Login/LoginService.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using git協作練習.Register;
+using git協作練習.Utility;
 
 namespace git協作練習.Login
 {
     internal class LoginService
     {
-        public void User()
+        public bool UserRegist(RegistModel model)
         {
-            Console.Write("請輸入帳號:");
-            string Account = Console.ReadLine();
-            Console.Write("請輸入密碼:");
-            string Password = Console.ReadLine();
-
+            UserModel userModel = new UserModel(model.Name,model.Account,model.Password,model.Address,"");
+            userModel.Account = model.Account;
+            userModel.Password = model.Password;
+            SendEmail(model.Account,"帳號註冊認證信","請輸入驗證碼");
+            return true;
         }
+
+        public void SendEmail(string account ,string subject,string content)
+        {
+            Mail.SendEmail(account, subject, content);
+        } 
 
 
 
