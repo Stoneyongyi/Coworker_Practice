5f7866e088ab5c6e2733c190afd2098f009a1464
DESKTOP-NQFA11O\YOI <j21801796147258@gmail.com>
2026-04-28 22:52:52 +0800

    新增登入流程

--- git協作練習/Program.cs
+++ git協作練習/Program.cs
@@ -6,11 +6,23 @@ using System.Reflection;
 
 
 
-Console.Write("請輸入帳號:");
-string Account = Console.ReadLine();
-Console.Write("請輸入密碼:");
-string Password = Console.ReadLine();
 
-LoginModel loginModel = new LoginModel(Account, Password);
-LoginService loginService = new LoginService();
-loginService.UserLogin(loginModel);
+
+
+bool userLogin = false;
+while (userLogin == false)
+{
+    Console.Write("請輸入帳號:");
+    string Account = Console.ReadLine();
+    Console.Write("請輸入密碼:");
+    string Password = Console.ReadLine();
+    LoginModel loginModel = new LoginModel(Account, Password);
+    LoginService loginService = new LoginService();
+    userLogin = loginService.UserLogin(loginModel);
+    Console.WriteLine("登入失敗,請重新登入");
+    if (userLogin == true)
+    {
+        Console.WriteLine("歡迎進入圖書管理系統");
+        break;
+    }
+}
